5be9375a026fca1c510a0706d12f6fd80a0da722
Prateek Gogia <prateekgogia@Prateeks-MacBook-Air.local>
2018-08-25 21:01:12 +0530

    Added Order schema

--- models/Order.js
+++ models/Order.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
 
-let Order = new mongoose.Schema({
+let OrderSchema = new mongoose.Schema({
     _id: {
         type: String,
         required: true
     },
-    name: {
+    date: {
+        type: Date,
+        required: true
+    },
+    customer_id: {
         type: String,
         required: true
     },
-    
+    items: {
+        type: Array,
+        required: true
+    }
 })
+
+module.exports = mongoose.model('Order', OrderSchema);
